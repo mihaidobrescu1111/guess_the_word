9d279d293913f972472d68bbb34a57cda5dc0304
Mihai Dobrescu <151914606+mihaidobrescu1111@users.noreply.github.com>
2024-08-26 21:02:18 +0300

    Delete docker directory

--- docker/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-../llama-server -m ./shieldgemma-2b-q5_k_m-imat.gguf -c 8192 -t 4 --port 8000 --host 127.0.0.1 --parallel 4 -cb &
-../llama-server -m ./gemma-2-2b-it-Q5_K_M.gguf -c 8192 -t 4 --port 7888 --host 127.0.0.1 --parallel 3 -cb &
-
-sleep 5
-
-cd trivia
-uvicorn app:app --port 7860 --host 0.0.0.0
